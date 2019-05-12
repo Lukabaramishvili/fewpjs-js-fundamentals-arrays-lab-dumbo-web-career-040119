f5a4e6e08a6029d37f72a1fa653484afff383d83
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-05-12 23:09:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
-append.push("Odie")
-prepend.unshift("Odie")
-removeLast.pop("Odie")
-removeFirst.shift("Odie")
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop("Odie");
+removeFirst.shift("Odie");
